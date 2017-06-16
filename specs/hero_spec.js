7d2c5cfc52f1ad6777088f9aa689c98fcaf00e81
Charlie Wood <charliemowood@gmail.com>
2017-06-16 23:34:16 +0100

    Add functionality hero should have health

--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -14,4 +14,19 @@ describe('Hero', function() {
   it('hero should have a name', function() {
     assert.strictEqual("Garry", hero.name);
   });
+
+  it('hero should have health', function() {
+    assert.strictEqual(100, hero.health);
+  });
+
+  xit('hero should have favorite food', function(done) {
+    assert.strictEqual("Banana", hero.favorite)
+  });
+
+  xit('hero should have a collection of tasks to complete', function(done) {
+    assert.strictEqual([], hero.collectionOfTasks)
+  });
+
+
+
 });
