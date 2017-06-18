6340849bf7ee429b61c984a264f556f4c5e0b5ab
Charlie Wood <charliemowood@gmail.com>
2017-06-18 22:26:20 +0100

    Add tests

--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -29,6 +29,20 @@ describe('Hero', function() {
     assert.deepEqual([], hero.collectionOfTasks)
   });
 
+  xit('hero should be able to eat food, and health should go up by the replenishment value', function() {
+  });
+
+  xit('When the hero eats food if it is their favourite food, their health should go up by 1.5 * value', function() {
+  });
+
+  xit('Hero should be able to sort their tasks by difficulty, urgency or reward', function() {
+  });
+
+  xit('A hero should be able to view tasks that are marked as completed or incomplete', function() {
+
+  });
+
+
 
 
 });
